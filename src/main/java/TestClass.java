8d6011574a508b661b05a12b72144727795c6977
avsjs <subbu.swaroop@gmail.com>
2019-03-19 14:51:52 +0200

    test

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,6 +1,8 @@
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println(" Test1 First change" );
+
+        System.out.println(" Test1 First change****" );
+        System.out.println("Second commit");
     }
 }
